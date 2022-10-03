4db240bfe5f5d21a1b81a55305b33b586d1b9ace
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-10-04 00:07:10 +0300

    Add SET instead autowired with constructor. Autowired Deth8 to chan

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,12 +1,23 @@
 package koschei.models;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
 
+  // @Autowired
+  private Deth8 deth;
+
+  @Autowired
+  public void setDeth8(Deth8 deth) {
+    this.deth = deth;
+  }
+
   @Override
   public String toString() {
-    return ", смерть Кощея на игле :( " + "";
+    return ", смерть Кощея на игле :( " + deth.toString();
   }
+
 }
