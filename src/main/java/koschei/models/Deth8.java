bab83914197187f3c49ec043d18880668dbbdd79
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-10-04 18:54:35 +0300

    remove autowired constructor

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Deth8 {
 
-  @Autowired
-  public Deth8() {
-  }
-  
   @Override
   public String toString() {
     return "";
